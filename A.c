d30e64c52e26524009595774e3b1e9b49f51d37f
dwtang <dwtang@umich.edu>
2015-01-10 20:40:27 -0500

    Add ggg

--- A.c
+++ A.c
@@ -3,6 +3,10 @@
 int main(){
 	int i = 1, k = 2; // variable declaration
 	printf("Hello git!\n");
+	printf("Hello git!\n");
+	printf("Hello git!\n");
+	printf("Hello git!\n");
+	printf("Hello git!\n");
 	printf("Hello world! %d, %d", i, k); // print it on display
 	return 0;
 }
